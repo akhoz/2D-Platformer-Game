8965a138e69452fd1629c7d2c739f653ad310639
akhoz <adrian25092005@gmail.com>
2023-12-06 08:56:30 -0600

    Flip player sprite animation and animation state implemented

--- game/Assets/Scripts/PlayerMovement.cs
+++ game/Assets/Scripts/PlayerMovement.cs
@@ -10,15 +10,18 @@ public class PlayerMovement : MonoBehaviour
     
     Vector2 moveInput;
     Rigidbody2D rb;
+    Animator anim;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     
     void Update()
     {
         Run();
+        FlipSprite();
     }
     
     void OnMove(InputValue value)
@@ -30,5 +33,18 @@ public class PlayerMovement : MonoBehaviour
     void Run()
     {
         rb.velocity = new Vector2(moveInput.x * speed, moveInput.y );
+        
+        bool isMoving = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon; //Mathf.Epsilons basically 0
+        anim.SetBool("isRunning", isMoving);
+    }
+
+    void FlipSprite()
+    {
+        bool isMoving = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon; //Mathf.Epsilons basically 0
+        
+        if (isMoving)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
 }
